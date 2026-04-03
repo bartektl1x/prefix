5381948323d026bc374d35931c88bf86d9e1086b
bartektl1x <108548699+bartektl1x@users.noreply.github.com>
2026-04-03 12:07:53 +0200

    Update test4.py

--- test4.py
+++ test4.py
@@ -35,3 +35,64 @@ class TestSourceTableConfig:
         # when / then
         with pytest.raises(ValueError):
             SourceTableConfig.model_validate(source_dict)
+
+###########################
+
+
+
+class TestPipelineConfig:
+
+    def test_valid_config_with_static_catalog(self):
+        # given
+        config_dict = {
+            "target_table_name": "my_table",
+            "target_table_schema": "staging",
+            "target_table_catalog": "gold_prod",
+            "source_tables": [{"name": "table1", "alias": "t1"}],
+            "custom_transformation": {
+                "python_path": "transformations.books",
+                "class_name": "FlattenBooks",
+            },
+        }
+
+        # when
+        config = PipelineConfig.model_validate(config_dict)
+
+        # then
+        assert config.target_table_catalog == "gold_prod"
+
+    def test_valid_config_with_dynamic_catalog(self):
+        # given
+        config_dict = {
+            "target_table_name": "my_table",
+            "target_table_schema": "staging",
+            "target_table_catalog": "{gold_catalog}",
+            "source_tables": [{"name": "table1", "alias": "t1"}],
+            "custom_transformation": {
+                "python_path": "transformations.books",
+                "class_name": "FlattenBooks",
+            },
+        }
+
+        # when
+        config = PipelineConfig.model_validate(config_dict)
+
+        # then
+        assert config.target_table_catalog == "{gold_catalog}"
+
+    def test_invalid_catalog_placeholder_fails(self):
+        # given
+        config_dict = {
+            "target_table_name": "my_table",
+            "target_table_schema": "staging",
+            "target_table_catalog": "{random_catalog}",
+            "source_tables": [{"name": "table1", "alias": "t1"}],
+            "custom_transformation": {
+                "python_path": "transformations.books",
+                "class_name": "FlattenBooks",
+            },
+        }
+
+        # when / then
+        with pytest.raises(ValueError):
+            PipelineConfig.model_validate(config_dict)
